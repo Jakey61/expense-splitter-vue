b192942a133336d8933562279684147151dd20d0
Jacob <jab6197@gmail.com>
2026-02-08 13:28:17 -0600

    Added card changed name

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="card">
+            <slot />
+    </main>
+</template>
+
+<style scoped>
+    .card{
+    width: min(600px, 100%);
+    background: #ff3f;
+    padding: 18px;
+    border-radius: 16px;
+    box-shadow: 0 10px 24px rgba(69, 70, 229, 0.2);
+}
+</style>
